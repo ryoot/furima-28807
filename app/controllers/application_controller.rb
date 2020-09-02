bdd523c12b5b8430d0496598b2f497f08b7bc0b7
ryoot <ryota.bsk.1202@gmail.com>
2020-09-02 12:38:02 +0900

    新規登録の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:nickname,:email,:encrypted_password,:first_name,:family_name,:first_name_kana,:family_name_kana,:birthday])
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_063641) do
+ActiveRecord::Schema.define(version: 2020_09_02_013927) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
