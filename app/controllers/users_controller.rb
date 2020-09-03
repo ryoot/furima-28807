06389bab0dad14bb74916a4cf635b2d05bfe86f2
ryoot <ryota.bsk.1202@gmail.com>
2020-09-03 16:25:02 +0900

    編集機能の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,11 @@
 class UsersController < ApplicationController
 
   def edit
-    @user = User.create(user_params)
-    @user.password_confirmation = user_params[:password]
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to user_registration_path
-      flash[:notice] = "新規登録が完了しました"
-    else
-      flash[:alert] = @user.errors.full_messages
-      redirect_to user_registration_path
-    end
+    
   end
 
   def update
-    current_user.update(user_params)
+    
   end
 
   private
