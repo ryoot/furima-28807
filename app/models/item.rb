e9fde322a58c216a1b8d31c75f64e51a631793ab
ryoot <ryota.bsk.1202@gmail.com>
2020-09-07 10:32:53 +0900

    テストコードの実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,20 +6,26 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_date
 
-  validates :text, :category_id, presence: true
+  validates :name, :text, presence: true
+
+  validates :price,presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+
+  validates :category_id, presence: true
   validates :category_id,  numericality: { other_than: 0 }
 
-  validates :text, :status_id, presence: true
+  validates :status_id, presence: true
   validates :status_id,  numericality: { other_than: 0 }
 
-  validates :text, :burden_shipping_id, presence: true
+  validates :burden_shipping_id, presence: true
   validates :burden_shipping_id,  numericality: { other_than: 0 }
 
-  validates :text, :prefecture_id, presence: true
+  validates :prefecture_id, presence: true
   validates :prefecture_id,  numericality: { other_than: 0 }
 
-  validates :text, :shipping_date_id, presence: true
+  validates :shipping_date_id, presence: true
   validates :shipping_date_id,  numericality: { other_than: 0 }
+
+  
 
 
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,40 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   describe Item do
-    describe '#create' do
       it "全て入力されていれば登録できる" do
-        item = FactoryBot.create(:item)
+        item = FactoryBot.build(:item)
         expect(item).to be_valid
       end
       it "nameが空なら登録できない" do
         item = FactoryBot.build(:item, name: nil)
         item.valid?
-        expect(item.errors.full_messages).to include("を入力してください")
+        expect(item.errors.full_messages).to include("Name can't be blank")
       end
       it "textが空なら登録できない" do
         item = FactoryBot.build(:item, text: nil)
         item.valid?
-        expect(item.errors.full_messages).to include("を入力してください")
+        expect(item.errors.full_messages).to include("Text can't be blank")
       end
       it "priceが空なら登録できない" do
         item = FactoryBot.build(:item, price: nil)
         item.valid?
-        expect(item.errors.full_messages).to include("を入力してください")
+        expect(item.errors.full_messages).to include("Price can't be blank")
       end
       it "priceがinteger以外なら登録できない" do
         item = FactoryBot.build(:item, price: "３００")
         item.valid?
-        expect(item.errors.full_messages).to include("は数値で入力してください")
+        expect(item.errors.full_messages).to include("Price is not a number")
       end
       it "priceが300円未満なら登録できない" do
         item = FactoryBot.build(:item, price: "290")
         item.valid?
-        expect(item.errors.full_messages).to include("は300以上の値にしてください")
+        expect(item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it "priceが9999999円よりも高額なら登録できない" do
         item = FactoryBot.build(:item, price: "19999999")
         item.valid?
-        expect(item.errors.full_messages).to include("は9999999以下の値にしてください")
+        expect(item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
     end
   end
