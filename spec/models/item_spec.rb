e434bb2c100547d13cc70125f333a8bf63613f30
ryoot <ryota.bsk.1202@gmail.com>
2020-09-07 14:11:56 +0900

    修正３

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,49 +2,51 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   describe Item do
+    before do
+      @item = FactoryBot.build(:item)
+    end
       it "全て入力されていれば登録できる" do
-        item = FactoryBot.build(:item)
-        expect(item).to be_valid
+        expect(@item).to be_valid
       end
       it "nameが空なら登録できない" do
-        item = FactoryBot.build(:item, name: nil)
-        item.valid?
-        expect(item.errors.full_messages).to include("Name can't be blank")
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
       end
       it "textが空なら登録できない" do
-        item = FactoryBot.build(:item, text: nil)
-        item.valid?
-        expect(item.errors.full_messages).to include("Text can't be blank")
+        @item.text = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Text can't be blank")
       end
       it "category_idが空なら登録できない" do
-        item = FactoryBot.build(:item, category_id: nil)
-        item.valid?
-        expect(item.errors.full_messages).to include("Category can't be blank")
+        @item.category_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it "statusが空なら登録できない" do
-        item = FactoryBot.build(:item, status_id: nil)
-        item.valid?
-        expect(item.errors.full_messages).to include("Status can't be blank")
+        @item.status_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
       end
       it "burden_shipping_idが空なら登録できない" do
-        item = FactoryBot.build(:item, burden_shipping_id: nil)
-        item.valid?
-        expect(item.errors.full_messages).to include("Burden shipping can't be blank")
+        @item.burden_shipping_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Burden shipping can't be blank")
       end
       it "prefecture_idが空なら登録できない" do
-        item = FactoryBot.build(:item, prefecture_id: nil)
-        item.valid?
-        expect(item.errors.full_messages).to include("Prefecture can't be blank")
+        @item.prefecture_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it "shipping_date_idが空なら登録できない" do
-        item = FactoryBot.build(:item, shipping_date_id: nil)
-        item.valid?
-        expect(item.errors.full_messages).to include("Shipping date can't be blank")
+        @item.shipping_date_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping date can't be blank")
       end
       it "priceが空なら登録できない" do
-        item = FactoryBot.build(:item, price: nil)
-        item.valid?
-        expect(item.errors.full_messages).to include("Price can't be blank")
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it "priceがinteger以外なら登録できない" do
         item = FactoryBot.build(:item, price: "３００")
