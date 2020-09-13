b8b4cd3990d9814345e514fad34c4431cac2d629
ryoot <ryota.bsk.1202@gmail.com>
2020-09-13 18:07:08 +0900

    単体モデルテストコード

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,9 +1,1 @@
-FactoryBot.define do
-  factory :order do
-    card_number           {"4242424242424242"}
-    card_year             {"25"}
-    card_month            {"12"}
-    card_cvc             {"111"}
-  end
-  end
 
